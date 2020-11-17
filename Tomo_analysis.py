97fd562316b4f445f6a147e43acd110396fd0207
Jaianthv <43813831+jaianthv@users.noreply.github.com>
2020-11-17 20:55:48 +0100

    Update Tomo_analysis.py

--- Tomo_analysis.py
+++ Tomo_analysis.py
@@ -153,9 +153,12 @@ def multiprocess_thickness_calculator(**args):
         
     if "save_image" in Keys:
         save_image_index = Keys.index("save_image")
+        save_image = "yes"
         if Values[save_image_index] == "yes":
             os.chdir(Values[folder_index])
             os.mkdir("Processed")
+    else:
+        save_image = "No"
         
     
     images = select_images(folder=Values[folder_index],image_type=Values[image_type_index])
@@ -179,14 +182,15 @@ def multiprocess_thickness_calculator(**args):
     data_thickness = []
     data_std_dev = []
     filename_data=open("data_list.txt","a")
-    for i in range(len(Divided_array)):
+    for i in range(0,len(Divided_array)):
         #temp_data = []
         #temp_slices =[]
         #temp_thickness = []
         #temp_std =[]
         print (Divided_array[i])
         start = Divided_array[i][0]
-        end = Divided_array[i][len(Divided_array[i])-1]
+        end = Divided_array[i][len(Divided_array[i])-1]+6
+        #end = Divided_array[i+1][5]
         array = store_as_nd_array(images[int(start):int(end)])
         thickness_image = ps.filters.local_thickness(array)
         thickness_image = np.array(thickness_image, dtype=np.uint16)
@@ -200,7 +204,7 @@ def multiprocess_thickness_calculator(**args):
             filename_data.write("%i %f %f\n"%(index_z[j],data_thickness[j],data_std[j]))
             
             
-    if Values[save_image_index] == "yes":
+    if save_image == "yes":
        os.chdir(Values[folder_index])
        os.chdir("Processed")
        for jj in range(len(images)):
@@ -219,32 +223,8 @@ def multiprocess_thickness_calculator(**args):
         
         #check if the file is empty
         # if not write
-        '''
-        if len(filename_data.readlines()) == 0 or len(filename_data.readlines()) == 1:
-            for j in range(len(Divided_array[i])-1):
-            filename_data.write("%i %f %f\n"%(Divided_array[i][j],data_thickness[j],data_std[j]))
-        # if filled - find slice no that matches with given slice number
-        if len(filename_data.readlines()) != 0 or len(filename_data.readlines()) != 1:
-            lines = file_name_data.readlines()
-            x = [line.split(" ")[0] for line in lines]
-            x= np.array(x,dtype=np.uint16)
-            for j in range(len(Divided_array[i])-1):
-                if (Divided_array[i][j] in x):
-                    pos = x.index(Divided_array)
-                    temp_line = lines[pos]
-                    temp_line = temp_line.replace("\n"," ")
-                    temp_line_split = temp_line.split(" ")
-                    temp_z_value = int(temp_line_split[0])
-                    temp_thickness = int(temp_line_split[1])
-                    temp_std = int(temp_line_split[2])
-                    Divided_array[i][j] = (Divided_array[i][j] + temp_z_value)/2
-                    data_thickness[j] = (data_thickness[j] + temp_thickness)/2
-                    data_std[j] = (data_std[j]+temp_std)/2
-                    
-                    
-                    
-                filename_data.write("%i %f %f\n"%(Divided_array[i][j],data_thickness[j],data_std[j]))
-         ''''           
+        
+            
      
 
     filename_data.close()
@@ -304,21 +284,39 @@ def create_vtk(**args):
             coordinate_z = z_value
             coordinate_x = int(coordinate[0][1])  #change in coordinate
             coordinate_y = int(coordinate[0][0]) 
-            scalar = 
+            #scalar = 
 
 
             
 def create_split_array(len_of_files,overlap):
     Divide = len_of_files/200
     overlap =int(overlap)
-    #print (No_of_images)
-    Nos=np.linspace(0,No_of_images-1,No_of_images)
+    Nos=np.linspace(0,len_of_files-1,len_of_files)
+    
     Divided_array = np.array_split(Nos,round(Divide))
-    for i in range(0,len(Divided_array)-2):
-        temp_len = len(Divided_array)
-        Divided_array[i].append(Divided_array[i][temp_len-(overlap+1):temp_len-1])
+
+    '''
+    print (type(Divided_array[0]))
+    
+    
+
+    for i in range(0,len(Divided_array)-1):
+        temp_len = len(Divided_array[i])
+        
+        
+        start = 0
+        end = overlap
+        list_temp = Divided_array[i+1][start:end]
+        print(list_temp)
+        for ii in range(len(list_temp)):
+            
+            np.append(Divided_array[i],list_temp[ii])
+            print (len(Divided_array[i]))
+            
+    print (Divided_array)
+    '''
                                       
- return Divided_array
+    return Divided_array
 
     #take frist 5 lines in array average
     
